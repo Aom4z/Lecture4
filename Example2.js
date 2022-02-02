797767f0b37cc0ef1cd716751be769c5ff97c2d4
Aom4z <6306021630056@fitm.kmutnb.ac.th>
2022-02-02 13:02:38 +0700

    update Example2.js

--- Example2.js
+++ Example2.js
@@ -1,28 +1,28 @@
-const equation = (value,convert) =>{
-    let a = value.split(" ");
-    let total = convert(a[0]);
-    for (let n=1;n<a.length;n++) {
-        if(a[n] == '+') total += convert(a[n+1]);
-        if(a[n] == '-') total -= convert(a[n+1]);
-        if(a[n] == '*') total *= convert(a[n+1]);
-        if(a[n] == '/') total /= convert(a[n+1]);
+const equation = (Value,convert) =>{
+    let num = Value.split(" ");
+    let total = convert(num[0]);
+    for (let i =1;i<num.length;i++) {
+        if(num[i] == '+') total += convert(num[i+1]);
+        if(num[i] == '-') total -= convert(num[i+1]);
+        if(num[i] == '*') total *= convert(num[i+1]);
+        if(num[i] == '/') total /= convert(num[i+1]);
     }
     return total;
 }
 const convert = (text) => {
     Number.isNaN(Number.parseInt(text));
-    let number=0;
+    let number= 0;
     number = parseInt(text);
-    if((text == 'one')) number = 1;
-    else if((text == 'two')) number = 2;
-    else if((text == 'three')) number = 3;
-    else if((text == 'four')) number = 4;
-    else if((text == 'five')) number = 5;
-    else if((text == 'six')) number = 6;
-    else if((text == 'seven')) number = 7;
-    else if((text == 'eight')) number = 8;
-    else if((text == 'nine')) number = 9;
-    else if((text == 'zero')) number = 0;
+    if ((text == 'one')) number = 1;
+    else if ((text == 'two')) number = 2;
+    else if ((text == 'three')) number = 3;
+    else if ((text == 'four')) number = 4;
+    else if ((text == 'five')) number = 5;
+    else if ((text == 'six')) number = 6;
+    else if ((text == 'seven')) number = 7;
+    else if ((text == 'eight')) number = 8;
+    else if ((text == 'nine')) number = 9;
+    else if ((text == 'zero')) number = 0;
     return number;
 }
 console.log("1+1 = "+equation('1 + 1',convert));
